6d21a9643b3368e2b21e66f69c20a6f4247ecaca
Michael Hartisch <michaelhartisch@gmx.de>
2026-02-06 13:36:52 +0100

    Updated FindHighs.cmake files, due to changes in directory structure of Highs

--- cmake/modules/FindHighs.cmake
+++ cmake/modules/FindHighs.cmake
@@ -2,10 +2,13 @@ if (NOT HIGHS_ROOT_DIR)
     message(STATUS "No path to the root directory of Highs was provided. I now try to find it myself:")
     file(GLOB_RECURSE HIGHS_DIRS_A "/opt/HiGHS/src/Highs.h")
     file(GLOB_RECURSE HIGHS_DIRS_B "/Applications/HiGHS/src/Highs.h")
+    file(GLOB_RECURSE HIGHS_DIRS_C "/opt/HiGHS/highs/Highs.h")
+    file(GLOB_RECURSE HIGHS_DIRS_D "/Applications/HiGHS/highs/Highs.h")
     #file(GLOB_RECURSE HiGHS_DIRS_C "**/HiGHS/src/Highs.h")
     list(APPEND HIGHS_DIRS ${HIGHS_DIRS_A})
     list(APPEND HIGHS_DIRS ${HIGHS_DIRS_B})
     list(APPEND HIGHS_DIRS ${HIGHS_DIRS_C})
+    list(APPEND HIGHS_DIRS ${HIGHS_DIRS_D})
     if(HIGHS_DIRS)
         message(STATUS "I found these directories:")
         foreach(HIGHS_DIR ${HIGHS_DIRS})
@@ -16,7 +19,7 @@ if (NOT HIGHS_ROOT_DIR)
             get_filename_component(HIGHS_ROOT_DIR "${HIGHS_ROOT_DIR}" DIRECTORY)  #/opt/HiGHS/src
             get_filename_component(HIGHS_ROOT_DIR "${HIGHS_ROOT_DIR}" DIRECTORY)  #/opt/HiGHS
 
-            if(NOT EXISTS "${HIGHS_ROOT_DIR}/src/Highs.h")
+            if(NOT EXISTS "${HIGHS_ROOT_DIR}/src/Highs.h" AND NOT EXISTS "${HIGHS_ROOT_DIR}/highs/Highs.h")
                 continue()
             else()
                 message(STATUS "I did select this folder: ${HIGHS_ROOT_DIR}")
@@ -31,15 +34,15 @@ if (NOT HIGHS_ROOT_DIR)
         message(STATUS "Found root. HIGHS_ROOT_DIR: ${HIGHS_ROOT_DIR}")
     endif()
 else()
-    if(NOT EXISTS "${HIGHS_ROOT_DIR}/src")
-        message(FATAL_ERROR "The provided HIGHS_ROOT_DIR does not seem to contain the folder 'src'. Maybe a typo?")
+    if(NOT EXISTS "${HIGHS_ROOT_DIR}/src" AND NOT EXISTS "${HIGHS_ROOT_DIR}/highs")
+        message(FATAL_ERROR "The provided HIGHS_ROOT_DIR neither seems to contain the folder 'src' nor the folder 'highs'.")
     endif()
 endif()
 
-message(STATUS "Looking for HIGHS include files in ${HIGHS_ROOT_DIR}/src and other paths")
+message(STATUS "Looking for HIGHS include files in ${HIGHS_ROOT_DIR}/src and ${HIGHS_ROOT_DIR}/highs and other paths")
 find_path(HIGHS_INCLUDE_DIR
   NAMES Highs.h
-  HINTS ${HIGHS_ROOT_DIR}/src
+  HINTS ${HIGHS_ROOT_DIR}/src ${HIGHS_ROOT_DIR}/highs
   PATHS ENV C_INCLUDE_PATH
         ENV C_PLUS_INCLUDE_PATH
         ENV INCLUDE_PATH
